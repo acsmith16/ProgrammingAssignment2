4c0d9f3b57d176a93f0e938bf1b4849406407f0a
acsmith16 <acsmith16@crimson.ua.edu>
2015-09-27 15:47:31 -0500

    Updated cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2: Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## default the return variable to null
+  inverseM<-NULL
+  
+  ## function to set the value of the matrix
+  set <- function(y){
+    x<<-y
+    inverseM<<- NULL
+  }
+  
+  ## returns the value of the input matrix
+  get <- function() {x}
+  
+  ## caches the inverted matrix after it has been found
+  setInverse<-function(inverse){
+    inverseM<<-inverse
+  }
+  
+  ## returns the value of the inverted matrix
+  getInverse<-function() inverseM
+  
+  ##store the functions to apply to the makeCacheMatrix function
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the matrix 
+## has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## calls the getInverse function in makeCacheMatrix and sets inverseM equal to the result      
+  inverseM <- x$getInverse()
+  
+  ##checkes if inverse has already been found and cached
+  if(!is.null(inverseM)){
+    message("retrieving cached data")
+    return (inverseM)
+  }
+  
+  ## retrieves the initial matrix
+  origionalM <-x$get()
+  
+  ## solves for the inverse matrix
+  inverseM<-solve(origionalM)
+  
+  ## calls the setInverse function so the inverse will be cached
+  x$setInverse(inverseM)
+  
+  ##returns the inverse
+  inverseM
+  
 }
+
